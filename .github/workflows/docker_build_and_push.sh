ddaff498bdacea4e3107474ac0bb971267c74826
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-08-29 00:10:02 -0600

    Migrated from Docker Hub builds to GHA.

---
+++ .github/workflows/docker_build_and_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker buildx build \
+    --platform ${ARCHITECTURE//-/\/} \
+    --output "type=image,push=true" \
+    --tag ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-${ARCHITECTURE//linux-/} \
+    --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+    --build-arg COMMIT_AUTHOR="$(git log -1 --pretty=format:'%ae')" \
+    --build-arg VCS_REF="${GITHUB_SHA}" \
+    --build-arg VCS_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}" \
+    --build-arg SMA_REF=`curl -sX GET "https://api.github.com/repos/mdhiggins/sickbeard_mp4_automator/commits/master" | jq '.sha' | tr -d '"'` \
+    --build-arg BASE_REF=`docker inspect hotio/radarr:musl --format '{{ index .Config.Labels "org.opencontainers.image.revision"}}'` \
+    --file ./Dockerfile ./
