cd2ef4f9f8c02e0d1ee17d7366af982be0e2d184
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-08-29 00:18:26 -0600

    Removed ARMv7 from the manifest.

--- .github/workflows/docker_manifest.sh
+++ .github/workflows/docker_manifest.sh
@@ -2,7 +2,6 @@
 
 docker manifest create ${GITHUB_REPOSITORY}:${GITHUB_SHA:0:7} \
     --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
-    --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
     --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
 docker manifest push ${GITHUB_REPOSITORY}:${GITHUB_SHA:0:7}
 
@@ -10,7 +9,6 @@ if [[ ${GITHUB_REF//refs\/heads\//} == "main" ]]
 then
     docker manifest create ${GITHUB_REPOSITORY}:latest \
         --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
         --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
     docker manifest push ${GITHUB_REPOSITORY}:latest
 
@@ -18,17 +16,12 @@ then
         --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64
     docker manifest push ${GITHUB_REPOSITORY}:amd64
 
-    docker manifest create ${GITHUB_REPOSITORY}:arm-v7 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7
-    docker manifest push ${GITHUB_REPOSITORY}:arm-v7
-
     docker manifest create ${GITHUB_REPOSITORY}:arm64 \
         --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
     docker manifest push ${GITHUB_REPOSITORY}:arm64
 else
     docker manifest create ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//} \
         --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-amd64 \
-        --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm-v7 \
         --amend ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}-${GITHUB_SHA:0:7}-arm64
     docker manifest push ${GITHUB_REPOSITORY}:${GITHUB_REF//refs\/heads\//}
 fi
